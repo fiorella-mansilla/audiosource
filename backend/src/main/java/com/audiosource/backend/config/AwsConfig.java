d668df2f136b6830356d5ba9fa896d04d513836a
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-05-13 16:12:36 +0200

    Finished implementation of env variables and refactored S3Client from AWS credentials to S3-Presigner

--- backend/src/main/java/com/audiosource/backend/config/AwsConfig.java
+++ backend/src/main/java/com/audiosource/backend/config/AwsConfig.java
@@ -1,31 +1,32 @@
 package com.audiosource.backend.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class AwsConfig {
 
-    @Value("${AWS_ACCESS_KEY_ID}")
-    private String awsAccessKeyId;
+    @Autowired
+    private Dotenv dotenv;
 
-    @Value("${AWS_SECRET_KEY}")
-    private String awsSecretKey;
-
-    private final String awsRegion = "eu-north-1";
-
+    /* Create an S3-Presigner using the default region and credentials. */
     @Bean
-    public S3Client s3Client() {
+    public S3Presigner s3Presigner() {
+
+        String awsAccessKeyId = dotenv.get("AWS_ACCESS_KEY_ID");
+        String awsSecretKey = dotenv.get("AWS_SECRET_KEY");
 
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(awsAccessKeyId, awsSecretKey);
 
-        return S3Client.builder()
-                .region(Region.of(awsRegion))
-                .credentialsProvider(() -> awsCredentials)
+        return S3Presigner.builder()
+                .region(Region.EU_NORTH_1)
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
                 .build();
     }
 }

a71c1bee71b8ae9135105bec9838d1552e1c6ea9
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-05-13 16:20:28 +0200

    Implemented the controller that will return the presigned url and the link from the file to upload to S3

--- backend/src/main/java/com/audiosource/backend/controller/S3Controller.java
+++ backend/src/main/java/com/audiosource/backend/controller/S3Controller.java
@@ -1,7 +1,33 @@
-package com.audiosource.backend.controllers;
+package com.audiosource.backend.controller;
 
+import com.audiosource.backend.service.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class S3Controller {
+
+    @Autowired
+    private S3Service s3Service;
+
+    @PostMapping("/s3/upload/signed_url")
+    public ResponseEntity<?> createPresignedPost(@RequestBody Map<String, String> request) {
+
+        try {
+            // All the put objects will be located in the `originals` S3 sub-bucket
+            String key = "originals/" + request.get("key");
+            String contentType = "audio/" + request.get("content_type");
+            Map<String, String> data = s3Service.createPresignedPost(key, contentType);
+            return ResponseEntity.ok().body(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Error generating the pre-signed URL");
+        }
+
+    }
 }

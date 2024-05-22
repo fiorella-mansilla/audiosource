2e4e1995960bd59f584d4d9ca7e28521d59b3911
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-05-22 15:25:27 +0200

    Implemented a controller for a GET request to list all objects from a S3 bucket

--- backend/src/main/java/com/audiosource/backend/controller/S3Controller.java
+++ backend/src/main/java/com/audiosource/backend/controller/S3Controller.java
@@ -1,21 +1,28 @@
 package com.audiosource.backend.controller;
 
+import com.audiosource.backend.dto.S3ObjectDto;
 import com.audiosource.backend.service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import software.amazon.awssdk.services.s3.model.S3Object;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/s3")
 public class S3Controller {
 
     @Autowired
     private S3Service s3Service;
 
-    @PostMapping("/s3/upload/signed_url")
+    @PostMapping("/upload/signed_url")
     public ResponseEntity<?> createPresignedPost(@RequestBody Map<String, String> request) {
 
         try {
@@ -30,5 +37,9 @@ public class S3Controller {
         }
     }
 
-
+    /* Lists all files from the specified S3 bucket. */
+    @GetMapping("/listObjects")
+    public List<S3ObjectDto> listObjects(@RequestParam String bucketName) {
+        return s3Service.listObjects(bucketName);
+    }
 }

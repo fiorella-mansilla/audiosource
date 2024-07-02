8bd6b03cda863f4d14cb2d0ff3dc8db9ae0f0d04
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-07-02 13:53:49 +0200

    Last commit

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,6 +1,12 @@
-
 const config = {
     API_BASE_URL: 'http://localhost:8080',
+    AWS: {
+        AccessKeyId: 'AKIAVRUVRLOTCJPIYQSW',
+        AWSSecretKey: '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',
+        BucketName: 'audiosource-files',
+        Region: "eu-north-1",
+    }
 };
 
 export default config;
+

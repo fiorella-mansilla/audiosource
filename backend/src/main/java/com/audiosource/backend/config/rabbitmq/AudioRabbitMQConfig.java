598436e769e434ba37c23a111a0fe3ae5888807a
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-08-28 16:57:12 +0200

    Implemented Message Queues with RabbitMQ Config

---
+++ backend/src/main/java/com/audiosource/backend/config/rabbitmq/AudioRabbitMQConfig.java
@@ -0,0 +1,37 @@
+package com.audiosource.backend.config.rabbitmq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AudioRabbitMQConfig {
+
+    // Constants for exchange and routing key
+    public static final String AUDIO_EXCHANGE_NAME = "audioFilesExchange";
+    public static final String AUDIO_ROUTING_KEY = "audio.routing.key";
+
+    // Queue name
+    public static final String AUDIO_FILES_QUEUE = "audioFilesQueue";
+
+    // Define Exchange
+    @Bean
+    public TopicExchange audioExchange() {
+        return new TopicExchange(AUDIO_EXCHANGE_NAME);
+    }
+
+    // Define Queue
+    @Bean
+    public Queue audioFilesQueue() {
+        return new Queue(AUDIO_FILES_QUEUE, true);
+    }
+
+    // Define Binding
+    @Bean
+    public Binding audioFilesBinding(Queue audioFilesQueue, TopicExchange audioExchange) {
+        return BindingBuilder.bind(audioFilesQueue).to(audioExchange).with(AUDIO_ROUTING_KEY);
+    }
+}

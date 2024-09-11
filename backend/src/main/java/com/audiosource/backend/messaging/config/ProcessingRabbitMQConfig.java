6c64acc3d79e42a4f3853f8689c02f16aba01775
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-09-11 09:27:08 +0200

    Updated configuration for ProcessedFiles queue

--- backend/src/main/java/com/audiosource/backend/messaging/config/ProcessingRabbitMQConfig.java
+++ backend/src/main/java/com/audiosource/backend/messaging/config/ProcessingRabbitMQConfig.java
@@ -4,43 +4,86 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProcessingRabbitMQConfig {
 
-    // Constants for exchange and routing key
-    public static final String PROCESSING_EXCHANGE_NAME = "processingExchange";
-    public static final String PROCESSED_ROUTING_KEY = "processed.routing.key";
-    public static final String ERROR_ROUTING_KEY = "error.routing.key";
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitmqHost;
 
-    // Queue Names
-    public static final String PROCESSED_FILES_QUEUE = "processedFilesQueue";
-    public static final String ERROR_QUEUE = "errorQueue";
+    @Value("${spring.rabbitmq.port}")
+    private int rabbitmqPort;
+
+    @Value("${spring.rabbitmq.username}")
+    private String rabbitmqUsername;
+
+    @Value("${spring.rabbitmq.password}")
+    private String rabbitmqPassword;
+
+    @Value("${processedFiles.queue.name}")
+    private String processedFilesQueueName;
+
+    @Value("${processedFiles.exchange.name}")
+    private String processedFilesExchangeName;
+
+    @Value("${processedFiles.routing.key}")
+    private String processedFilesRoutingKey;
+
+    @Value("${error.queue.name}")
+    private String errorQueueName;
+
+    @Value("${error.routing.key}")
+    private String errorRoutingKey;
 
     @Bean
     public DirectExchange processingExchange() {
-        return new DirectExchange(PROCESSING_EXCHANGE_NAME);
+        return new DirectExchange(processedFilesExchangeName);
     }
 
     @Bean
     public Queue processedFilesQueue() {
-        return new Queue(PROCESSED_FILES_QUEUE, true);
-    }
-
-    @Bean
-    public Queue errorQueue() {
-        return new Queue(ERROR_QUEUE, true);
+        return new Queue(processedFilesQueueName, true);
     }
 
     @Bean
     public Binding processedFilesBinding(Queue processedFilesQueue, DirectExchange processingExchange) {
-        return BindingBuilder.bind(processedFilesQueue).to(processingExchange).with(PROCESSED_ROUTING_KEY);
+        return BindingBuilder.bind(processedFilesQueue).to(processingExchange).with(processedFilesRoutingKey);
+    }
+
+    @Bean
+    public Queue errorQueue() {
+        return new Queue(errorQueueName, true);
     }
 
     @Bean
     public Binding errorBinding(Queue errorQueue, DirectExchange processingExchange) {
-        return BindingBuilder.bind(errorQueue).to(processingExchange).with(ERROR_ROUTING_KEY);
+        return BindingBuilder.bind(errorQueue).to(processingExchange).with(errorRoutingKey);
+    }
+
+    /* ConnectionFactory bean to establish a connection to RabbitMQ */
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setHost(rabbitmqHost);
+        factory.setPort(rabbitmqPort);
+        factory.setUsername(rabbitmqUsername);
+        factory.setPassword(rabbitmqPassword);
+        return factory;
+    }
+
+    /* Message Serialization for producers : Ensures that outgoing messages are automatically converted
+from Java objects to JSON when sent to RabbitMQ.*/
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(new Jackson2JsonMessageConverter());
+        return template;
     }
 }

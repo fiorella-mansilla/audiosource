b5f333f448b05d4891a2b6cef569b0c2b2bf1a8c
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-09-11 09:37:18 +0200

    Implemented function for publishing messages to Processed Files queue

--- backend/src/main/java/com/audiosource/backend/messaging/producer/ProcessedFilesProducerService.java
+++ backend/src/main/java/com/audiosource/backend/messaging/producer/ProcessedFilesProducerService.java
@@ -1,4 +1,30 @@
 package com.audiosource.backend.messaging.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProcessedFilesProducerService {
+    private final RabbitTemplate rabbitTemplate;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProcessedFilesProducerService.class);
+
+    @Value("${processedFiles.exchange.name}")
+    private String processedFilesExchangeName;
+
+    @Value("${processedFiles.routing.key}")
+    private String processedFilesRoutingKey;
+
+    @Autowired
+    public ProcessedFilesProducerService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void publishProcessedFileNotification(String processedFileMessage) {
+        rabbitTemplate.convertAndSend(processedFilesExchangeName, processedFilesRoutingKey, processedFileMessage);
+        LOGGER.info("Published message to ProcessedFilesQueue: {}", processedFileMessage);
+    }
 }

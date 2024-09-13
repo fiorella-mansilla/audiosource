6d17f4d325b4f0f47e601e7c36933e7e5cbb8bca
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-09-13 10:26:54 +0200

    Refactored NotificationService by adding a createEmailMessage method to encapsulate email cretion logic and improved error handling in sendEmailToUser function

--- backend/src/main/java/com/audiosource/backend/service/notification/NotificationService.java
+++ backend/src/main/java/com/audiosource/backend/service/notification/NotificationService.java
@@ -1,6 +1,9 @@
 package com.audiosource.backend.service.notification;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,18 +11,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class NotificationService {
     private static final String EMAIL_SENDER = "audiosource.project@gmail.com";
-    private static final String EMAIL_SUBJECT = "Your audio file is ready for download!";
+    private static final String EMAIL_SUBJECT = "AudioSource : Your audio file is ready for download!";
+    private static final Logger LOGGER = LoggerFactory.getLogger(NotificationService.class);
+
+    private final JavaMailSender mailSender;
 
     @Autowired
-    private JavaMailSender mailSender;
+    public NotificationService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
-    public void sendEmailToUser(String to, String body) {
+    public boolean sendEmailToUser(String to, String downloadUrl) {
+        SimpleMailMessage message = createSimpleMessage(to, downloadUrl);
+        try {
+            mailSender.send(message);
+            return true;
+        } catch (MailException e) {
+            LOGGER.error("Failed to send email to {}: {}", to, e.getMessage());
+            return false;
+        }
+    }
+
+    private SimpleMailMessage createSimpleMessage(String to, String downloadUrl) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(EMAIL_SENDER);
         message.setTo(to);
         message.setSubject(EMAIL_SUBJECT);
-        message.setText(body);
-
-        mailSender.send(message);
+        message.setText("You can download your separated files at: " + downloadUrl);
+        return message;
     }
 }

441d176fafe93feea7cfcd63a298f55c0274042a
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2025-02-22 12:32:07 +0100

    Modify local host vite

--- backend/src/main/java/com/audiosource/backend/config/CorsConfig.java
+++ backend/src/main/java/com/audiosource/backend/config/CorsConfig.java
@@ -2,17 +2,15 @@ package com.audiosource.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000")
+                .allowedOriginPatterns("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
